0fe54c7963fcf56ad1e64a21598bc840e3ff8b5c
Diomedes Co <diomedesjrco@gmail.com>
2025-05-27 18:43:37 +0800

    Register and Login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 function App() {
   return (
     <div className="flex min-h-screen">
       <Router>
-        <Navbar />
-        <Dashboard />
         <Routes>
-          <Route />
+          <Route
+            path="/"
+            element={<Login />}
+          />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
         </Routes>
       </Router>
     </div>
